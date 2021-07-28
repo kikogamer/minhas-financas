7bfab8547a6b317c25450533601aceb7b16d5e85
Ronaldo Carneiro <kikogamer@gmail.com>
2021-07-28 14:14:08 -0300

    adicionado configuração de api

---
+++ src/minhas-financas-api/Configuration/ApiConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace minhas_financas_api.Configuration
+{
+    public static class ApiConfiguration
+    {
+        public static IServiceCollection AddWebApiConfiguration(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseWebApiConfiguration(this IApplicationBuilder app)
+        {
+            app.UseHttpsRedirection();
+            
+            app.UseRouting();
+
+            return app;
+        }
+    }
+}
--- src/minhas-financas-api/Startup.cs
+++ src/minhas-financas-api/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using minhas_financas_api.Configuration;
 
 namespace minhas_financas_api
 {
@@ -29,7 +30,8 @@ namespace minhas_financas_api
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddWebApiConfiguration();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minhas Finanças API", Version = "v1" });
@@ -46,11 +48,9 @@ namespace minhas_financas_api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "minhas_financas_api v1"));
             }
 
-            app.UseHttpsRedirection();
+            app.UseWebApiConfiguration();
 
-            app.UseRouting();
-
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
