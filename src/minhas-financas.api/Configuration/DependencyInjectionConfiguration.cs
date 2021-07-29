db54d757d2d5a22713691aad3f2e5ac0bb3fcf50
Ronaldo Carneiro <kikogamer@gmail.com>
2021-07-29 12:37:30 -0300

    finalizado configuração do notificador

--- src/minhas-financas.api/Configuration/DependencyInjectionConfiguration.cs
+++ src/minhas-financas.api/Configuration/DependencyInjectionConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using minhas_financas.business.Interfaces;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace minhas_financas.api.Configuration
@@ -8,6 +9,8 @@ namespace minhas_financas.api.Configuration
     {
         public static IServiceCollection ResolveDependencies(this IServiceCollection services)
         {
+            services.AddScoped<INotificador, Notificador>();
+
             services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 
             return services;
