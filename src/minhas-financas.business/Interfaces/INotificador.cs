84ceecefe9759400beb7580a3e8b04bc9e4c8c61
Ronaldo Carneiro <kikogamer@gmail.com>
2021-07-29 12:26:46 -0300

    criando notificador

---
+++ src/minhas-financas.business/Interfaces/INotificador.cs
@@ -0,0 +1,12 @@
+﻿using minhas_financas.business.Notificacoes;
+using System.Collections.Generic;
+
+namespace minhas_financas.business.Interfaces
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}
