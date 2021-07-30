f5c53d954ab9be8740929d0b253cdb759b8c94a2
Ronaldo Carneiro <kikogamer@gmail.com>
2021-07-30 13:13:23 -0300

    adicionado configuração de jwt token e finalizado registro de usuário.

--- src/minhas-financas.api/V1/Controllers/UsuariosController.cs
+++ src/minhas-financas.api/V1/Controllers/UsuariosController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using minhas_financas.api.Controllers;
+using minhas_financas.api.Extensions;
 using minhas_financas.api.V1.ViewModels;
 using minhas_financas.business.Interfaces;
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace minhas_financas.api.V1.Controllers
@@ -14,13 +21,16 @@ namespace minhas_financas.api.V1.Controllers
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
 
         public UsuariosController(INotificador notificador,
                                   SignInManager<IdentityUser> signInManager,
-                                  UserManager<IdentityUser> userManager) : base(notificador)
+                                  UserManager<IdentityUser> userManager,
+                                  IOptions<AppSettings> appSettings) : base(notificador)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpGet("{id}")]
@@ -30,7 +40,7 @@ namespace minhas_financas.api.V1.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> Registrar(CreateUserViewModel createUser)
+        public async Task<ActionResult> Registrar(UserViewModel createUser)
         {
             if (!ModelState.IsValid) return CustomResponse(ModelState);
 
@@ -46,8 +56,7 @@ namespace minhas_financas.api.V1.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, false);
-                //return CustomResponse(await GerarJwt(user.Email));
-                return CustomCreatedResponse(nameof(GetUser), user.Id, createUser);
+                return CustomCreatedResponse(nameof(GetUser), user.Id, await GerarJwt(user.Email));
             }
 
             foreach (var error in result.Errors)
@@ -57,5 +66,56 @@ namespace minhas_financas.api.V1.Controllers
 
             return CustomResponse(createUser);
         }
+
+        private async Task<LoginResponseViewModel> GerarJwt(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            var claims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+
+            foreach (var userRole in userRoles)
+            {
+                claims.Add(new Claim("role", userRole));
+            }
+
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Emissor,
+                Audience = _appSettings.ValidoEm,
+                Subject = identityClaims,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            var encondedToken = tokenHandler.WriteToken(token);
+
+            var response = new LoginResponseViewModel
+            {
+                AccessToken = encondedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UserToken = new UserTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new ClaimViewModel { Type = c.Type, Value = c.Value })
+                }
+            };
+
+            return response;
+        }
+
+        private static long ToUnixEpochDate(DateTime date)
+            => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
     }
 }
